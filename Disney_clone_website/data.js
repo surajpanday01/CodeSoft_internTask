b62c09e00be3a107508c0c9e3ee05515e6b6f775
SURAJ PANDEY <142084737+surajpanday01@users.noreply.github.com>
2023-11-11 17:56:38 +0000

    first comit

---
+++ Disney_clone_website/data.js
@@ -0,0 +1,39 @@
+let movies = [
+    {
+        name: 'Trailer',
+        des: 'this movie shows amazing action sequences and love triangle and drama .this is india  theatre in 2023!',
+        image: 'images/slider.jpg'
+    },
+
+    {
+        name: ' The Freelancer',
+        des: ' this movie shows amazing action sequences and love triangle and drama .this is india  theatre in 2023!',
+        image: 'images/slider-1.jpg'
+    },
+    {
+        name: 'Shaitan Series',
+        des: '  this movie shows amazing action sequences and love triangle and drama .this is india  theatre in 2023!',
+        image: 'images/slider-2.jpg'
+    },
+    {
+        name: ' Apurva',
+        des: 'this movie shows amazing action sequences and love triangle and drama .this is india  theatre in 2023!',
+        image: 'images/slider-3.jpg'
+    },
+    
+    {
+        name: 'Commando-4',
+        des: 'this movie shows amazing action sequences and love triangle and drama .this is india  theatre in 2023!',
+        image: 'images/slider-4.jpg'
+    },
+    {
+        name: 'Atithi',
+        des: 'this movie shows amazing action sequences and love triangle and drama .this is india  theatre in 2023!',
+        image: 'images/slider-5.jpg'
+    },
+    {
+        name: 'lsiddhart malhotra',
+        des: 'this movie shows amazing action sequences and love triangle and drama .this is india  theatre in 2023!',
+        image: 'images/slider-6.jpg'
+    }
+]
